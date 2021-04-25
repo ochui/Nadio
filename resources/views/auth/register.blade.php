103511b74c5b860188c830a0d36c60d67b0de5f1
Ochui Princewill <ochuiprincewill411@gmail.com>
2021-04-25 18:44:52 +0100

    auth page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('frontend.layouts.app')
+@extends('frontend.layouts.auth')
 
 @section('content')
 
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel Real Estate') }}</title>
+    <title>Nadio Real Estate Agency</title>
 
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
