03379f7573d948276bf0361f8ded5e9b0e484e21
Ochui Princewill <ochuiprincewill411@gmail.com>
2021-04-25 18:50:41 +0100

    meta tags

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -9,6 +9,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Nadio Real Estate Agency</title>
+    <meta name=description content="We provide a more reliable alternative in the property acquisition and management process.
+    Rent, lease, sell or Purchase landed property in Nigeria."/>
 
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
